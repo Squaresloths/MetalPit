b0a663f618fcc4a8ad62d61b932818e2f8efc0c6
SquareSloth <raduadumitru70@gmail.com>
2025-07-08 15:26:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ if (!isset($_SESSION["user"])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
-    <div class="container">
+<body style="text-align: center;">
+    <div class="container" style="background-color: #ff0000; border-style: groove; border-width: 5px; border-color: black; margin-top: 20px;">
         <h1>Welcome to the Dashboard</h1>
         <a href="logout.php" class="btn btn-warning">Logout</a>
     </div>
